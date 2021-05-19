153539dbefca5f224d78e6e1dea67e3e60bdafe8
Mohtasim Bellah <mohtasim22@gmail.com>
2021-05-19 21:09:42 +0600

    [Backend] Current Weather Api implemented. [Description] Weather service, controller and helper has been implemented. We are converting the response from 'openweather' to a custom json response for our own usage.
    Signed-off-by: Mohtasim Bellah <mohtasim22@gmail.com>

---
+++ Backend/thinkificweather/src/main/java/com/mohtasimtest/thinkificweather/contracts/ApiContracts.java
@@ -0,0 +1,8 @@
+package com.mohtasimtest.thinkificweather.contracts;
+
+public class ApiContracts {
+
+    public static final String API_KEY = "8b75e4a1286e11bd31e05e78da315ef4";
+    public static final String UNIT = "metric";
+
+}
---
+++ Backend/thinkificweather/src/main/java/com/mohtasimtest/thinkificweather/services/CurrentWeatherServiceHelper.java
@@ -0,0 +1,39 @@
+package com.mohtasimtest.thinkificweather.services;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.mohtasimtest.thinkificweather.models.CurrentWeather;
+import org.springframework.stereotype.Component;
+
+import static com.mohtasimtest.thinkificweather.contracts.WeatherContracts.*;
+import static com.mohtasimtest.thinkificweather.contracts.WeatherContracts.TEMPERATURE;
+
+@Component
+public class CurrentWeatherServiceHelper {
+
+    public void mapResponseToCurrentWeatherPOJO(CurrentWeather currentWeather, JsonNode rootNode) {
+        currentWeather.setWeatherMain(rootNode.path(WEATHER).get(0).path(WEATHER_MAIN).asText());
+        currentWeather.setWeatherDescription(rootNode.path(WEATHER).get(0).path(WEATHER_DESCRIPTION).asText());
+        currentWeather.setWeatherIcon(rootNode.path(WEATHER).get(0).path(WEATHER_ICON).asText());
+
+        currentWeather.setCityName(rootNode.path(CITY_NAME).asText());
+        currentWeather.setCityIdentifier(rootNode.path(CITY_IDETIFIER).asInt());
+        currentWeather.setTimezone(rootNode.path(TIMEZONE).asLong());
+
+        currentWeather.setCountryCode(rootNode.path(SYSTEM).path(COUNTRY_CODE).asText());
+        currentWeather.setSunrise(rootNode.path(SYSTEM).path(SUNRISE).asLong());
+        currentWeather.setSunset(rootNode.path(SYSTEM).path(SUNSET).asLong());
+
+        currentWeather.setDt(rootNode.path(DATETIME).asLong());
+
+        currentWeather.setWindSpeed(rootNode.path(WIND).path(WIND_SPEED).asDouble());
+        currentWeather.setWindDeg(rootNode.path(WIND).path(WIND_DEG).asDouble());
+        currentWeather.setWindGust(rootNode.path(WIND).path(WIND_GUST).asDouble());
+
+        currentWeather.setVisibility(rootNode.path(VISIBILITY).asInt());
+
+        currentWeather.setHumidity(rootNode.path(MAIN).path(HUMIDITY).asInt());
+        currentWeather.setPressure(rootNode.path(MAIN).path(PRESSURE).asInt());
+        currentWeather.setCurrentTemperatureFeelsLike(rootNode.path(MAIN).path(CURRENT_TEMPERATURE_FEELS_LIKE).asDouble());
+        currentWeather.setTemperature(rootNode.path(MAIN).path(TEMPERATURE).asDouble());
+    }
+}
