21f232cc07371696a6cf60676f4ed2455ea9b812
Mohtasim Bellah <mohtasim22@gmail.com>
2021-05-19 16:52:44 +0600

    [Backend] Spring-Boot project initialization for backend. [Framework] Spring-Boot [Agenda1] Project dependecies added while initializing the project. [Agenda2] Model class implemented for weather api implementation.
    Signed-off-by: Mohtasim Bellah <mohtasim22@gmail.com>

---
+++ Backend/thinkificweather/src/main/java/com/mohtasimtest/thinkificweather/ThinkificweatherApplication.java
@@ -0,0 +1,13 @@
+package com.mohtasimtest.thinkificweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinkificweatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThinkificweatherApplication.class, args);
+	}
+
+}
