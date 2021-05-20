9a8da83cc9f8de0dc5526aa7fb63d0d798cb5bad
Mohtasim Bellah <mohtasim22@gmail.com>
2021-05-20 18:39:41 +0600

    [BackEnd] Custom exception handling in Runtime implemented. [Description] For wrong city name, the exception would occur in runtime. So the front end would need a exception message that is easier to parse and show to user.
    Signed-off-by: Mohtasim Bellah <mohtasim22@gmail.com>

---
+++ Backend/thinkificweather/src/main/java/com/mohtasimtest/thinkificweather/exceptions/CustomResponseEntityExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.mohtasimtest.thinkificweather.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+@RestController
+public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler
+    public ResponseEntity<Object> handleInvalidCityNameException(InvalidCityNameException invalidCityNameException,
+                                                                 WebRequest request) {
+        InvalidCityNameExceptionResponse invalidCityNameExceptionResponse = new InvalidCityNameExceptionResponse(invalidCityNameException.getMessage());
+        return new ResponseEntity(invalidCityNameExceptionResponse, HttpStatus.BAD_REQUEST);
+    }
+}
---
+++ Backend/thinkificweather/src/main/java/com/mohtasimtest/thinkificweather/exceptions/InvalidCityNameExceptionResponse.java
@@ -0,0 +1,18 @@
+package com.mohtasimtest.thinkificweather.exceptions;
+
+public class InvalidCityNameExceptionResponse {
+
+    private String error_message;
+
+    public InvalidCityNameExceptionResponse(String error_message) {
+        this.error_message = error_message;
+    }
+
+    public String getError_message() {
+        return error_message;
+    }
+
+    public void setError_message(String error_message) {
+        this.error_message = error_message;
+    }
+}
