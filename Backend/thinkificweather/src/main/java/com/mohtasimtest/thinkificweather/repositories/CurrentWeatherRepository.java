73515c19c0dc9eb82221552fee0836b79279a496
Mohtasim Bellah <mohtasim22@gmail.com>
2021-05-20 18:37:16 +0600

    [Backend] Implementing 10 min cache in terms of city. [Description] City model class designed and added. City Repository added to integrade with CRUD ops. Business Logic implemented with weather service so that if same city is queried within 10 mins then it would return the cached city result.
    Signed-off-by: Mohtasim Bellah <mohtasim22@gmail.com>

--- Backend/thinkificweather/src/main/java/com/mohtasimtest/thinkificweather/repositories/CurrentWeatherRepository.java
+++ Backend/thinkificweather/src/main/java/com/mohtasimtest/thinkificweather/repositories/CurrentWeatherRepository.java
@@ -7,4 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CurrentWeatherRepository extends CrudRepository<CurrentWeather, Long> {
 
+    CurrentWeather getWeatherByCityName(String cityName);
 }
