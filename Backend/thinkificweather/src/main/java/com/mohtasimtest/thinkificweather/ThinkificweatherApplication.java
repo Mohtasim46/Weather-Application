0d7d7dc130369404d8cdf07497e4c2295dbaa98c
Mohtasim Bellah <mohtasim22@gmail.com>
2021-05-29 01:48:25 +0600

    [Backend] Bean added for BCrypt library.
    Signed-off-by: Mohtasim Bellah <mohtasim22@gmail.com>

--- Backend/thinkificweather/src/main/java/com/mohtasimtest/thinkificweather/ThinkificweatherApplication.java
+++ Backend/thinkificweather/src/main/java/com/mohtasimtest/thinkificweather/ThinkificweatherApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -12,6 +13,11 @@ public class ThinkificweatherApplication {
 	@Bean
 	RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
 		return restTemplateBuilder.build();
+	}
+
+	@Bean
+	BCryptPasswordEncoder bCryptPasswordEncoder(){
+		return new BCryptPasswordEncoder();
 	}
 
 	public static void main(String[] args) {
