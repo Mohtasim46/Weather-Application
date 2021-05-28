271e595fad2ef745005deda866cfa71ac3bad20b
Mohtasim Bellah <mohtasim22@gmail.com>
2021-05-28 19:25:56 +0600

    [Backend] Weather api handling to avoid unwanted situation.
    Signed-off-by: Mohtasim Bellah <mohtasim22@gmail.com>

--- Backend/thinkificweather/src/main/java/com/mohtasimtest/thinkificweather/controllers/CurrentWeatherController.java
+++ Backend/thinkificweather/src/main/java/com/mohtasimtest/thinkificweather/controllers/CurrentWeatherController.java
@@ -1,10 +1,13 @@
 package com.mohtasimtest.thinkificweather.controllers;
 
+import com.mohtasimtest.thinkificweather.exceptions.InvalidCityNameException;
 import com.mohtasimtest.thinkificweather.models.CurrentWeather;
 import com.mohtasimtest.thinkificweather.services.CurrentWeatherService;
+import com.mohtasimtest.thinkificweather.services.ErrorValidationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +20,9 @@ public class CurrentWeatherController {
     @Autowired
     private CurrentWeatherService currentWeatherService;
 
+    @Autowired
+    private ErrorValidationService errorValidationService;
+
     @GetMapping("/{cityName}")
     public ResponseEntity<?> getCurrentWeatherByCity(@PathVariable String cityName) {
 
@@ -24,4 +30,11 @@ public class CurrentWeatherController {
 
         return new ResponseEntity<CurrentWeather>(currentWeather, HttpStatus.OK);
     }
+
+    @GetMapping("/")
+    public ResponseEntity<?> getWeather() {
+        throw new InvalidCityNameException("Please type in a valid city name.");
+    }
+
+
 }
