2a3d7d0f8984a63e6dbfb1ba21ed5a405fcf547d
Mohtasim Bellah <mohtasim22@gmail.com>
2021-05-29 01:50:57 +0600

    [Backend] Customer model, form validator, repository and controller implementations.
    Signed-off-by: Mohtasim Bellah <mohtasim22@gmail.com>

--- Backend/thinkificweather/src/main/java/com/mohtasimtest/thinkificweather/contracts/SecurityContracts.java
+++ Backend/thinkificweather/src/main/java/com/mohtasimtest/thinkificweather/contracts/SecurityContracts.java
@@ -1,2 +1,13 @@
-package com.mohtasimtest.thinkificweather.contracts;public class SecurityContracts {
+package com.mohtasimtest.thinkificweather.contracts;
+
+public class SecurityContracts {
+
+    public static final String SING_UP_URL = "/api/customers/**";
+    public static final String H2_URL = "h2-console/**";
+
+    public static final String SECRET_KEY = "SecretKeyToGenerateJWTs";
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "Authorization";
+    public static final long EXPIRATION_TIME = 120_000;
+
 }
--- Backend/thinkificweather/src/main/java/com/mohtasimtest/thinkificweather/controllers/CustomerController.java
+++ Backend/thinkificweather/src/main/java/com/mohtasimtest/thinkificweather/controllers/CustomerController.java
@@ -1,12 +1,19 @@
 package com.mohtasimtest.thinkificweather.controllers;
 
 import com.mohtasimtest.thinkificweather.models.Customer;
+import com.mohtasimtest.thinkificweather.payload.JwtLoginSuccessResponse;
+import com.mohtasimtest.thinkificweather.payload.LoginRequest;
+import com.mohtasimtest.thinkificweather.security.JwtTokenProvider;
 import com.mohtasimtest.thinkificweather.services.CustomerService;
 import com.mohtasimtest.thinkificweather.services.ErrorValidationService;
 import com.mohtasimtest.thinkificweather.validator.CustomerValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,8 +22,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+import static com.mohtasimtest.thinkificweather.contracts.SecurityContracts.TOKEN_PREFIX;
+
 @RestController
-@RequestMapping("api/customer")
+@RequestMapping("api/customers")
 public class CustomerController {
 
     @Autowired
@@ -27,6 +36,12 @@ public class CustomerController {
 
     @Autowired
     private CustomerService customerService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
 
     @PostMapping("/register")
     public ResponseEntity<?> registerCustomer(@Valid @RequestBody Customer customer, BindingResult result) {
@@ -39,6 +54,24 @@ public class CustomerController {
         Customer newCustomer = customerService.saveCustomer(customer);
 
         return new ResponseEntity<Customer>(newCustomer, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateCustomer(@Valid @RequestBody LoginRequest loginRequest, BindingResult result) {
+
+        ResponseEntity<?> errorMap = errorValidationService.getErrorMapAfterValidation(result);
+        if(errorMap != null)return errorMap;
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = TOKEN_PREFIX + jwtTokenProvider.generateJwtToken(authentication);
+        return ResponseEntity.ok(new JwtLoginSuccessResponse(true, jwt));
     }
 
 
