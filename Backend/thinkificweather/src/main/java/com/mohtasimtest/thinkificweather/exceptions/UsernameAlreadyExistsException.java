d33f024c78343c5af890dcfaad5ae13c9b38b00f
Mohtasim Bellah <mohtasim22@gmail.com>
2021-05-29 01:46:44 +0600

    [Backend] Custom user authentication exceptions has been implemented to catch exceptions in runtime. [Description] If username already exists and invalid login payload exceptions has been added.
    Signed-off-by: Mohtasim Bellah <mohtasim22@gmail.com>

--- Backend/thinkificweather/src/main/java/com/mohtasimtest/thinkificweather/exceptions/UsernameAlreadyExistsException.java
+++ Backend/thinkificweather/src/main/java/com/mohtasimtest/thinkificweather/exceptions/UsernameAlreadyExistsException.java
@@ -1,2 +1,11 @@
-package com.mohtasimtest.thinkificweather.exceptions;public class UsernameAlreadyExistsException {
+package com.mohtasimtest.thinkificweather.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class UsernameAlreadyExistsException extends RuntimeException{
+    public UsernameAlreadyExistsException(String message) {
+        super(message);
+    }
 }
