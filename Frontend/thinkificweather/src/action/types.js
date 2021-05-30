180ede2b00e44ba032c3921965e7041e06d7e455
Mohtasim Bellah <mohtasim22@gmail.com>
2021-05-31 00:43:58 +0600

    [Frontend] Implemented the whole security or authentication flow. [Description] Added the landing page. Resolved token handling with the redux store for handling different scenarios when user is logged in/out. Fixed some bugs.
    Signed-off-by: Mohtasim Bellah <mohtasim22@gmail.com>

--- Frontend/thinkificweather/src/action/types.js
+++ Frontend/thinkificweather/src/action/types.js
@@ -1,2 +1,4 @@
 export const GET_ERRORS = "GET_ERRORS";
 export const GET_WEATHER = "GET_WEATHER";
+
+export const SET_CURRENT_USER = "SET_CURRENT_USER"; 
--- Frontend/thinkificweather/src/reducers/index.js
+++ Frontend/thinkificweather/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux"
 import errorreducer from "./errorreducer"
+import securityreducer from "./securityreducer";
 import weatherreducer from "./weatherreducer"
 
 
 export default combineReducers({
     weather: weatherreducer,
     errors: errorreducer,
+    security: securityreducer,
 });
